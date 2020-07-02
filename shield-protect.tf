55c08bb7cc4dc7d736b7775e5aea52b1a227489a
Naveen Bezawada <naveenb29@gmail.com>
2020-07-01 19:02:53 -0500

    discover VPC info and create shield protection

---
+++ shield-protect.tf
@@ -0,0 +1,13 @@
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+
+resource "aws_eip" "myeip" {
+  vpc = true
+}
+
+resource "aws_shield_protection" "foo" {
+  name         = "${var.name}"
+  resource_arn = "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:eip-allocation/${aws_eip.myeip.id}"
+}
